f7ea0adce0f264d4068f378e7a9f7e51d4a7bb75
adib82 <adibfirmansyah27@gmail.com>
2023-07-21 14:34:28 +0700

    membuat view menu, class admin dan kasir

--- src/tubesapotek/TubesApotek.java
+++ src/tubesapotek/TubesApotek.java
@@ -29,7 +29,7 @@ public class TubesApotek {
         System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        // Implementasi proses cek login menggunakan koneksi ke database
+        //proses cek login menggunakan koneksi ke database
         if (db.authenticateAdmin(username, password)) {
             isAdmin = true;
             isLoggedIn = true;
@@ -45,9 +45,36 @@ public class TubesApotek {
 
     public void menuKasir() {
         System.out.println("===MENU Kasir===");
-        System.out.println("1. Tambah Penjualan Obat");
-        System.out.println("2. Tambah Detail Penjualan Obat");
-        // Tambahkan kode untuk masing-masing pilihan menu kasir
+        System.out.println("1. Penjualan Obat");       
+        System.out.println("2. Detail Penjualan Obat");
+        System.out.print("Pilih: ");
+        int menu = scanner.nextInt();
+          switch(menu) {
+          case 1:
+            System.out.println("==Penjualan Obat==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            kasir ks1 = new kasir(menu);
+            
+            break;
+          case 2:
+            System.out.println("==Detail Penjualan Obat==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            kasir ks2 = new kasir(menu);
+            break;
+          default:
+            System.out.println("tidur");
+        }
+      
     }
 
     public void menuAdmin() {
@@ -57,6 +84,65 @@ public class TubesApotek {
         System.out.println("3. Supplier");
         System.out.println("4. Obat");
         System.out.println("5. Kategori");
-        // Tambahkan kode untuk masing-masing pilihan menu admin
+        System.out.print("Pilih: ");
+        
+        int menu = scanner.nextInt();
+        switch(menu) {
+        case 1:
+            System.out.println("==Pembelian Obat==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            admin adm1 = new admin(menu);
+            
+            break;
+        case 2:
+            System.out.println("==Detail Penjualan Obat==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            admin adm2 = new admin(menu);
+            break;
+        case 3:
+            System.out.println("==Supplier==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            admin adm3 = new admin(menu);
+            break;
+        case 4:
+            System.out.println("==Obat==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            admin adm4 = new admin(menu);
+            break;
+        case 5:
+            System.out.println("==Kategori==");
+            System.out.println("1. Tambah");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");      
+            System.out.println("4. View"); 
+            System.out.print("Pilih: ");
+            menu = scanner.nextInt();
+            admin adm5 = new admin(menu);
+            break;
+        default:
+            System.out.println("tidur");
+        
+        }
     }
+
 }
---
+++ src/tubesapotek/kasir.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tubesapotek;
+
+/**
+ *
+ * @author ADIB FIRMANSYAH
+ */
+public class kasir {
+    public kasir(int pilih){
+        System.out.println("kasir");
+    }
+}
